b183ca26d4580005a2db8cf58674ab19e9ad4151
Daniel Posch <dcposch@dcpos.ch>
2011-06-05 01:24:10 -0700

    Basic trace routing works now.
    Currently drag/drop, not click/escape.
    Checks intersection, highlights all traces under cursor.
    Adds all traces to a simple data struct.
    
    Next:
    * Basic keyboard shortcuts, including click/escape
    * Basic pads.
    * Netlist import/export.

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
+//Dan Posch
+//June 2011
+//WPCB 
+//
+
+
+//model
+traces = new pcb_traces();
+
 
 //globals
-var vp = document.getElementById('viewport');
-var ctx;
-var width, height;
+var vp = document.getElementById('viewport'); //viewport (<canvas>)
+var ctx; //canvas context
+var width, height; //viewport size, px
+var mx, my; //mouse location
+var dstartx, dstarty;
+dstartx = dstarty = null;
+
+//ui state
+var tool = null;
+var current_trace = null;
+
 
 //constants
 var grid_spacing = 20; //px
+var snap_to_grid = true;
+
+var color_background = '#000';
+var color_grid = '#333';
+var color_trace = '#bb0';
+var color_trace_highlight = '#ddd';
+
+
 
 function view_clear(){
-    ctx.fillStyle='#000';
+    ctx.fillStyle=color_background;
     ctx.fillRect(0,0,width,height);
 
     //draw gridlines
-    ctx.strokeStyle='#333';
+    ctx.strokeStyle=color_grid;
     ctx.lineWidth=1;
     ctx.beginPath();
     for(var x = 0; x < width; x += grid_spacing){
@@ -26,9 +51,12 @@ function view_clear(){
     ctx.stroke();
 }
 
-function view_draw_cursor(x,y){
-    x = Math.floor((x+grid_spacing/2)/grid_spacing)*grid_spacing;
-    y = Math.floor((y+grid_spacing/2)/grid_spacing)*grid_spacing;
+function view_draw_crosshair(x,y,snap_to_grid){
+    if(snap_to_grid){
+        loc = snap_location(x,y);
+        x = loc[0];
+        y = loc[1];
+    }
 
     ctx.lineWidth=1;
     ctx.strokeStyle='#ff0';
@@ -40,6 +68,147 @@ function view_draw_cursor(x,y){
     ctx.stroke();
 }
 
+function view_redraw(){
+    view_clear();
+    if(tool=='traces')
+        view_draw_crosshair(mx,my,true);
+    else if(tool==null)
+        view_draw_crosshair(mx,my,false);
+
+    //draw traces
+    for(var i in traces.traces){
+        draw_trace(traces.traces[i], color_trace);
+    }
+
+    //highlight traces
+    loc = snap_location(mx,my);
+    ts = traces.find_intersections(loc[0],loc[1]);
+    console.log(ts);
+    for(var i in ts){
+        draw_trace(ts[i], color_trace_highlight);
+    }
+
+    if(current_trace != null)
+        draw_trace(current_trace,color_trace_highlight);
+
+    //draw pads
+    //TODO
+}
+
+
+
+function set_tool(t){
+    tool = t;
+    $('#toolbox li').removeClass('selected');
+    if(t != null)
+        $("#"+t).addClass('selected');
+}
+
+function snap_location(x,y){
+    x = Math.floor((x+grid_spacing/2)/grid_spacing)*grid_spacing;
+    y = Math.floor((y+grid_spacing/2)/grid_spacing)*grid_spacing;
+    return [x,y];
+}
+
+function snap_direction(){
+    var dx = mx - dstartx;
+    var dy = my - dstarty;
+    if(Math.abs(dx) > 2*Math.abs(dy)){
+        //horizontal
+        dy = 0;
+    }
+    else if(Math.abs(dy) > 2*Math.abs(dx)){
+        //vertical
+        dx = 0;
+    }
+    else{
+        //diagonal
+        var davg = (Math.abs(dx)+Math.abs(dy))/2;
+        dx = (dx > 0) ? davg : -davg;
+        dy = (dy > 0) ? davg : -davg;
+    }
+    return [dstartx + dx, dstarty + dy];
+}
+
+function draw_trace(trace, color){
+    ctx.lineWidth = 4.0;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(trace[0], trace[1]);
+    ctx.lineTo(trace[2], trace[3]);
+
+    ctx.stroke();
+}
+
+function tool_start(){
+
+}
+function tool_update(){
+    if(tool == "traces"){
+        var msnap = snap_direction();
+        current_trace = [dstartx,dstarty,msnap[0],msnap[1]];
+    }
+}
+function tool_finish(){
+    if(tool=="traces"){
+        if(current_trace != null)
+            traces.add_trace(
+                current_trace[0],
+                current_trace[1],
+                current_trace[2],
+                current_trace[3]);
+        current_trace = null;
+    }
+}
+
+
+
+function setup_menus(){
+    $('#toolbox li').click(function(evt){
+        if(evt.currentTarget.id != tool)
+            set_tool(evt.currentTarget.id);
+        else
+            set_tool(null);
+    });
+}
+
+function setup_vp_mouse(){
+
+    //set up mouse events
+    $(vp).mousemove(function(evt){
+        mx = evt.pageX - $(vp).offset().left;
+        my = evt.pageY - $(vp).offset().top;
+
+        if(dstartx != null)
+            tool_update();
+        view_redraw();
+        });
+    $(vp).mousedown(function(evt){
+        mx = evt.pageX - $(vp).offset().left;
+        my = evt.pageY - $(vp).offset().top;
+        var loc = snap_location(mx,my);
+        dstartx = loc[0];
+        dstarty = loc[1];
+
+        tool_start();
+        tool_update();
+        view_redraw();
+        });
+    $(vp).mouseup(function(evt){
+        mx = evt.pageX - $(vp).offset().left;
+        my = evt.pageY - $(vp).offset().top;
+
+        tool_finish();
+        view_redraw();
+
+        dstartx = dstarty = null;
+        });
+}
+
+
+
+
 $(function(){
     
     //setup header links
@@ -47,6 +216,8 @@ $(function(){
     //set up model
 
     //set up controller
+    setup_menus();
+    setup_vp_mouse();
 
 
     //load default design
@@ -57,12 +228,4 @@ $(function(){
     ctx.translate(0.5,0.5);
     view_clear();
 
-
-    //set up mouse events
-    $(vp).mousemove(function(evt){
-        var mx = evt.pageX - $(vp).offset().left;
-        var my = evt.pageY - $(vp).offset().top;
-        view_clear();
-        view_draw_cursor(mx,my);
-        });
 });
--- pcb.js
+++ pcb.js
@@ -13,3 +13,57 @@
 // * User interaction (see controller)
 // * Display (see view)
 //
+
+function pcb_traces() {
+
+    this.traces = [];
+
+    this.find_trace = function(x1,y1,x2,y2){
+        for(var i = 0; i < this.traces.length; i++){
+            if(
+                this.traces[i][0] == x1 &&
+                this.traces[i][1] == y1 &&
+                this.traces[i][2] == x2 &&
+                this.traces[i][3] == y2){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    this.add_trace = function(x1,y1,x2,y2){
+        if(this.find_trace(x1,y1,x2,y2) > -1)
+            throw("pcb_trace: attempted double add.");
+        this.traces.push([x1,y1,x2,y2]);
+    }
+
+    this.find_intersections = function(x,y){
+        var ret = [];
+
+        for(var i in this.traces){
+            var t = this.traces[i];
+            x1 = t[0];
+            y1 = t[1];
+            x2 = t[2];
+            y2 = t[3];
+
+            var tx = (x - x1)/(x2-x1);
+            if(!isNaN(tx)){
+                if(tx < 0) continue;
+                if(tx > 1) continue;
+            }
+            var ty = (y - y1)/(y2-y1);
+            if(isNaN(ty) || isNaN(tx) || tx == ty)
+               ret.push(t);
+        }
+
+        return ret;
+    }
+
+    this.remove_trace = function(x1,y1,x2,y2){
+        var ix = this.find_trace(x1,y1,x2,y2);
+        if(ix < 0)
+            throw("pcb_trace: trace not found");
+        this.traces.removeAt(ix);
+    }
+}
