fc999800dcee0a3a9ff5095ebfb56061e40c542f
DC <dcposch@dcpos.ch>
2013-04-24 00:54:57 -0400

    Added Battery and Power charts

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 (function(){
 
-Ember.Handlebars.registerBoundHelper("num", function(num){
+/*** Ember ***/
+Ember.Handlebars.registerBoundHelper('num', function(num){
     if(num == null){
-        return "-";
+        return '-'
     }
-    return num.toFixed(2);
-});
-Ember.Handlebars.registerHelper("debug", function(optionalValue) {
-  console.log("Current Context");
-  console.log("====================");
-  console.log(this);
- 
+    return num.toFixed(2)
+})
+Ember.Handlebars.registerHelper('debug', function(optionalValue) {
+  console.log('Current Context:')
+  console.log(this)
   if (optionalValue) {
-    console.log("Value");
-    console.log("====================");
-    console.log(optionalValue);
+    console.log('Value:')
+    console.log(optionalValue)
   }
-});
+})
 
 App = Ember.Application.create()
 
@@ -31,15 +29,50 @@ App.Car = Ember.Object.extend({
     }.property('distM')
 })
 
-var car = App.Car.create()
+var car = window.fuu = App.Car.create()
+car.cells = []
 App.IndexRoute = Ember.Route.extend({
     model: function(){
         return car
     }
 })
 
+
+/*** D3 ***/
+var battery = batteryChart()
+    .width(250)
+    .height(350)
+var power = powerChart()
+function update(){
+    d3.select('#battery').datum(car.cells).call(battery);
+    d3.select('#power').datum(car).call(power);
+}
+
+/*** Mock data ***/
+for(var i = 0; i < 35; i++){
+    car.cells[i] = {
+        voltage: 3.5,
+        temperature: 48
+    }
+}
 setInterval(function(){
     car.set('speedKph', 100+Math.random())
     car.set('distM', car.get('distM')+1)
+
+    // battery
+    var d = Math.random()-0.5;
+    for(var i = 0; i < 35; i++){
+        car.cells[i].voltage += (Math.random()-0.5)*0.001 + d*0.008+(i-15)*0.00001;
+        car.cells[i].temperature += (Math.random()-0.5)*0.02 + d*0.07; 
+    }
+
+    // power
+    car.arrayCurrent = -8 + Math.random();
+    car.motorCurrent = 4 + 8*Math.random();
+    car.busCurrent = car.arrayCurrent + car.motorCurrent;
+    car.busVoltage = 0;
+    for(var i = 0; i < 35; i++) car.busVoltage += car.cells[i].voltage;
+
+    update();
 }, 50)
 })()
---
+++ battery.js
@@ -0,0 +1,105 @@
+
+/**
+ * Shows cell voltages and temperatures for a LiPo battery pack.
+ * See http://bost.ocks.org/mike/chart/ for the general implementation idea.
+ */
+function batteryChart(){
+    // Configuration
+    var width = 200, height = 200;
+    var margin = {top:10, right:10, bottom:20, left:30};
+    my.width = function(w){
+        if(w === undefined) return width;
+        width = w;
+        return this;
+    }
+    my.height = function(h){
+        if(h === undefined) return height;
+        height = h;
+        return this;
+    }
+
+    // Rendering
+    function createAxes(svg, xscale, yscale){
+        svg.attr('width', width+'px')
+            .attr('height', height+'px')
+    
+        var xaxis = svg.select('#xaxis')
+            .attr('transform', 'translate('+margin.left+','+(height-margin.bottom)+')')
+        xaxis.call(d3.svg.axis()
+            .orient('bottom')
+            .scale(xscale)
+            .tickValues([3,3.5,4]))
+        var ytop = -height+margin.top+margin.bottom - 5;
+        xaxis.append('line')
+            .attr('x1', xscale(2.8)).attr('y1',ytop)
+            .attr('x2', xscale(2.8)).attr('y2',0)
+            .style('stroke','gray')
+            .style('stroke-dasharray','1,1')
+        xaxis.append('line')
+            .attr('x1', xscale(4.2)).attr('y1',ytop)
+            .attr('x2', xscale(4.2)).attr('y2',0)
+            .style('stroke','gray')
+            .style('stroke-dasharray','1,1')
+
+        var yaxis = svg.select('#yaxis')
+            .attr('transform', 'translate('+margin.left+',0)')
+        yaxis.call(d3.svg.axis()
+            .orient('left')
+            .scale(yscale)
+            .tickValues([0,5,10,15,20,25,30,34])
+            .tickFormat(Math.floor))
+    }
+
+    function render(cells){
+        var svg = d3.select(this);
+        var xscale = d3.scale.linear()
+            .domain([2.5,4.5]) // volts
+            .range([0, width-margin.right-margin.left]) // bar width in pixels
+        var yscale = d3.scale.linear()
+            .domain([0,cells.length-0.5]) // index
+            .range([margin.top, height-margin.bottom]) // y in pixels
+        if(svg.selectAll('g')[0].length == 0){
+            svg.append('g').attr('id','xaxis').attr('class','axis');
+            svg.append('g').attr('id','yaxis').attr('class','axis');
+            createAxes(svg, xscale, yscale);
+
+            svg.append('g').attr('id','voltage');
+            svg.append('g').attr('id','temperature');
+        }
+
+        // bars (voltage)
+        var color = d3.scale.linear()
+            .domain([2.8, 3.1, 3.5, 3.9, 4.2]) // volts
+            .range(['red', '#ff4444', '#bbbb00', '#44bb44', 'green'])
+        var bars = svg.select('#voltage').selectAll('rect')
+            .data(cells)
+        bars.enter().append('rect')
+            .attr('x', margin.left)
+            .attr('height', 6)
+            .attr('y', function(d,i){return yscale(i)-3})
+        bars
+            .attr('width', function(d){return xscale(d.voltage)})
+            .attr('fill', function(d){return color(d.voltage)})
+
+        // text (temperature)
+        var tempColor = d3.scale.threshold()
+            .domain([50,60])
+            .range(['black', 'orange', 'red']);
+        var labels = svg.select('#temperature').selectAll('text')
+            .data(cells)
+        labels.enter().append('text')
+            .attr('dx', width)
+            .attr('dy', function(d,i){return yscale(i)})
+            .attr('width', 30)
+            .attr('height', 10)
+            .style('text-anchor', 'end');
+        labels
+            .attr('fill', function(d){return tempColor(d.temperature)})
+            .text(function(d){return Math.round(d.temperature)+' C'})
+    }
+    function my(selection){
+        selection.each(render);
+    };
+
+    return my;
+}
