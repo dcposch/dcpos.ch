2f07c6c2fee6e38403ecd2616f689c07044d5a77
DC <dcposch@dcpos.ch>
2022-04-08 20:02:01 -0700

    fix cert renewal script

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,7 +1,14 @@
+set -e
+
+echo "STOPPING NGINX"
+service nginx stop
+
 echo "RENEWING CERTS"
 certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d datpedia.org -d www.datpedia.org
 certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d scramble.io -d www.scramble.io
-certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d dcpos.ch -d p9.dcpos.ch -d blog.dcpos.ch -d www.dcpos.ch
-certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d batsign.al -d www.batsign.al
-certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d bazooka.city
+certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d dcpos.ch -d p9.dcpos.ch
+
+echo "STARTING NGINX"
+service nginx start
+
 echo "DONE"
