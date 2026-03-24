37fd4ba3d901911441c78ce69f9cfae472b3582f
DC <dc@dcpos.ch>
2026-03-24 02:30:56 -0700

    remove datpedia

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -4,7 +4,6 @@ echo "STOPPING NGINX"
 service nginx stop
 
 echo "RENEWING CERTS"
-certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d datpedia.org -d www.datpedia.org
 certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d scramble.io -d www.scramble.io
 certbot certonly --standalone --agree-tos --non-interactive --email dcposch@dcpos.ch -d dcpos.ch -d p9.dcpos.ch
 
